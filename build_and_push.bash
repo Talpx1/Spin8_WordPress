ea0dd107e7114401b6809b13a34c1d3614eb6707
Talpx1 <simo3401@gmail.com>
2023-12-22 00:20:45 +0100

    i managed to mess up, fix typo

--- build_and_push.bash
+++ build_and_push.bash
@@ -4,5 +4,5 @@ read -p "WP Version: " wpversion
 read -p "PHP Version: " phpversion
 read -p "WebServer Version: " webserverversion
 
-docker build -t talpx1/spin8_wordpress:$wpversion-php$phpversion-$vebserverversion . &&
-docker push talpx1/spin8_wordpress:$wpversion-php$phpversion-$vebserverversion
+docker build -t talpx1/spin8_wordpress:$wpversion-php$phpversion-$webserverversion . &&
+docker push talpx1/spin8_wordpress:$wpversion-php$phpversion-$webserverversion
