86345d51a96e339bcab15293d8f6c1eff02bf486
Talpx1 <simo3401@gmail.com>
2024-01-02 23:03:52 +0100

    improved build and push script

--- build_and_push.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "WP Version: " wpversion
-read -p "PHP Version: " phpversion
-read -p "WebServer Version: " webserverversion
-
-docker build -t talpx1/spin8_wordpress:$wpversion-php$phpversion-$webserverversion . &&
-docker push talpx1/spin8_wordpress:$wpversion-php$phpversion-$webserverversion
